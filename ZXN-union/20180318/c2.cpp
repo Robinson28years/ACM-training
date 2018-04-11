d4c1ea6ad9a7dbf5a020183a23c49e8a62413123
Robinson28years <bb.chen@outlook.com>
2018-04-11 10:25:09 +0800

    暂存

---
+++ ZXN-union/20180318/c2.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char a[200][200];
+
+void dfs(int r, int l) {
+	if (a[r][l]!='@') {
+		return ;
+	}
+	else {
+		a[r][l]='*';
+		dfs(r+1,l);
+		dfs(r,l+1);
+		dfs(r,l-1);
+		// dfs(r+1,l+1);
+		// dfs(r+1,l-1);
+		// dfs(r-1,l+1);
+		// dfs(r-1,l-1);
+		dfs(r-1,l);
+	}
+}
+
+int main()
+{
+	int x,y,k;
+	while (	std::cin >> x >> y && x!=0) {
+		k=0;
+		memset(a,'*',sizeof(a[0][0]));
+		for(int i=0; i<x; i++)
+		{
+			for(int j=0; j<y; j++)
+			{
+				std::cin >> a[i][j];
+			}
+		}
+		for(int i=0; i<x; i++)
+		{
+			for(int j=0; j<y; j++)
+			{
+				if (a[i][j]=='@') {
+					dfs(i,j);
+					k++;
+				}
+			}
+		}
+		std::cout << k << '\n';
+	}
+
+}
